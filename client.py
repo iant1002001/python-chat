448732b06096dffeff4009baff53814065ef4711
Ian Thomas <ithomas3@csub.edu>
2021-03-25 04:20:19 -0700

    Finished basic client for testing, working on GUI

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import socket
 import select
 import errno
+import sys
 
 # Set constants
 HEADER_LENGTH = 10
@@ -17,3 +18,41 @@ client_socket.setblocking(False)
 username = my_username.encode('utf-8')
 username_header = f"{len(username):<{HEADER_LENGTH}}".encode('utf-8')
 client_socket.send(username_header + username)
+
+# Loop through sending messages on the client
+while True:
+    message = input(f"{my_username} > ")
+
+    # If message isn't blank, encode it and send it
+    if message:
+        message = message.encode('utf-8')
+        message_header = f"{len(message) :< {HEADER_LENGTH}}".encode('utf-8')
+        client_socket.send(message_header + message)
+
+    try:
+        # Loop through receiving messages
+        while True:
+            # Receive username and message information 
+            # being broadcast by the server and decode it
+            username_header = client_socket.recv(HEADER_LENGTH)
+            if not len(username_header):
+                print("Connection closed by the server.")
+                sys.exit()
+            username_length = int(username_header.decode('utf-8'))
+            username = client_socket.recv(username_length).decode('utf-8')
+
+            message_header = client_socket.recv(HEADER_LENGTH)
+            message_length = int(message_header.decode('utf-8'))
+            message = client_socket.recv(message_length).decode('utf-8')
+
+            print(f"{username} > {message}")
+
+    except IOError as e:
+        if e.errno is not errno.EAGAIN and e.errno is not errno.EWOULDBLOCK:
+            print('Read error', str(e))
+            sys.exit()
+        continue
+
+    except Exception as e:
+        print('General error',str(e))
+        sys.exit()
